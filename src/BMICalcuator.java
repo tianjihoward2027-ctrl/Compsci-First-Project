2bfd2220e0f7f3db8e640c17c6ba4b2c1eb5ba10
tianjihoward2027-ctrl <tianjihoward2027@abc-net.edu.sv>
2025-09-04 06:58:58 -0600

    #

--- src/BMICalcuator.java
+++ src/BMICalcuator.java
@@ -2,6 +2,6 @@ import java.util.Scanner;
 
 public class BMICalcuator {
     public static void main(String[] args) throws Exception {
-        
+        System.out.println("Hello world");
     }
 }
