4720b3b84546bb4d365db9d21c211216baee97e3
tianjihoward2027-ctrl <tianjihoward2027@abc-net.edu.sv>
2025-09-04 07:34:09 -0600

    BMI calc (short for calculator)

--- src/BMICalcuator.java
+++ src/BMICalcuator.java
@@ -1,7 +1,42 @@
+import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class BMICalcuator {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello world");
+        Scanner input = new Scanner(System.in);
+        DecimalFormat oneDecimal = new DecimalFormat("#.0");
+
+        System.out.println("Enter height:");
+        String heightInput = input.nextLine();
+
+        double height;
+        while (true) {
+            try {
+                height = Double.parseDouble(heightInput);
+                break;
+            } catch (Exception e)  {
+                System.out.println("Invalid response. Enter height:");
+                input = new Scanner(System.in);
+                heightInput = input.nextLine();
+            }
+        }
+
+        System.out.println("Enter weight:");
+        input = new Scanner(System.in);
+        String weightInput = input.nextLine();
+
+        double weight;
+        while (true) {
+            try {
+                weight = Double.parseDouble(weightInput);
+                break;
+            } catch (Exception e)  {
+                System.out.println("Invalid response. Enter weight:");
+                input = new Scanner(System.in);
+                weightInput = input.nextLine();
+            }
+        }
+        double bmi = weight + height;
+        System.out.println("BMI is " + oneDecimal.format(bmi) + " kg/m^2");
     }
 }
